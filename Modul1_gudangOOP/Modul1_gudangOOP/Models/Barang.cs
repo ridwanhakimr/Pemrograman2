7859246c2b9408a3b94e4ee1e1b03f63a0d51418
Ridwan Hakim Ramadhan <ridwanhakimrmdhn@gmail.com>
2025-10-13 09:44:58 +0700

    Add files via upload

---
+++ Modul1_gudangOOP/Modul1_gudangOOP/Models/Barang.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul1_gudangOOP.Models
+{
+    public class Barang
+    {
+        public string KodeBarang;
+        public string NamaBarang;
+        public int JumlahStok;
+        public string Kategori;
+        public Barang(string kode, string nama, int stok, string kategori)
+        {
+            KodeBarang = kode;
+            NamaBarang = nama;
+            JumlahStok = stok;
+            Kategori = kategori;
+        }
+        public void TampilkanInfo()
+        {
+            Console.WriteLine($"[{KodeBarang}] {NamaBarang} - Stok: {JumlahStok}, Kategori: {Kategori}");
+        }
+        public string StatusStok()
+        {
+            return JumlahStok > 50 ? "Stok Aman" : "Perlu Reorder";
+        }
+        public Barang()
+        {
+            KodeBarang = "UNKNOWN";
+            NamaBarang = "Barang Baru";
+            JumlahStok = 0;
+            Kategori = "Umum";
+        }
+    }
+}
---
+++ Modul1_gudangOOP/Modul1_gudangOOP/Program.cs
@@ -0,0 +1,35 @@
+﻿using Modul1_gudangOOP.Models;
+using System;
+class Program
+{
+    static void Main()
+    {
+        Barang b1 = new Barang("BRG001", "Karton", 120, "Kemasan");
+        Barang b2 = new Barang("BRG002", "Bubble Wrap", 40, "Pelindung");
+
+        // Membuat array dan mengisi dengan objek Barang
+        Barang[] daftarBarang = new Barang[2];
+        daftarBarang[0] = b1;
+        daftarBarang[1] = b2;
+
+        // Menampilkan info semua barang dalam array
+        foreach (var barang in daftarBarang)
+        {
+            barang.TampilkanInfo();
+            Console.WriteLine($"Status: {barang.StatusStok()}");
+        }
+
+        Barang b3 = new Barang();
+        Console.Write("Masukkan Nama Barang: ");
+        b3.NamaBarang = Console.ReadLine();
+        Console.Write("Masukkan Kode Barang: ");
+        b3.KodeBarang = Console.ReadLine();
+        Console.Write("Masukkan Jumlah Stok: ");
+        Console.Write("Masukkan Jumlah Stok: ");
+        b3.JumlahStok = int.Parse(Console.ReadLine());
+        Console.Write("Masukkan Kategori: ");
+        b3.Kategori = Console.ReadLine();
+        b3.TampilkanInfo();
+        Console.WriteLine($"Status: {b3.StatusStok()}");
+    }
+}
